16d6e747308956b1a5744081c86121e4bd095476
dubaolei <dubaolei@jkcrm.cn>
2022-07-01 11:37:46 +0800

    Java8

--- src/main/java/dubaolei/Java8/JavaEight.java
+++ src/main/java/dubaolei/Java8/JavaEight.java
@@ -2,9 +2,7 @@ package dubaolei.Java8;
 
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.sound.midi.Soundbank;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
@@ -19,15 +17,16 @@ import java.util.stream.Collectors;
  * @Description TODO
  * @createTime 2022年06月29日 23:06:00
  */
-@RestController
-public class ThreadLocalTest {
+
+public class JavaEight {
 
     public static void main(String[] args) {
         List<Student> students = Lists.newArrayList();
         students.add(new Student("1", "张三", LocalDate.of(2009, Month.JANUARY, 1), 12, 12.123));
         students.add(new Student("2", "李四", LocalDate.of(2010, Month.FEBRUARY, 2), 11, 22.123));
         students.add(new Student("3", "王五", LocalDate.of(2011, Month.MARCH, 3), 10, 32.123));
-        students.add(new Student("3", "杜六", LocalDate.of(2012, Month.MARCH, 4), 9, 42.123));
+        students.add(new Student("4", "杜六", LocalDate.of(2012, Month.MARCH, 3), 11, 42.123));
+        students.add(new Student("5", "赵六", LocalDate.of(2012, Month.MARCH, 3), 10, 52.123));
 
         /**
          * 中间操作
@@ -50,7 +49,6 @@ public class ThreadLocalTest {
         // 元素数量   counting
         Long count = students.stream().collect(Collectors.counting());
         System.out.println(count);
-
         // 平均值  averagingDouble、averagingInt、averagingLong
         Double averagingDouble = students.stream().collect(Collectors.averagingDouble(Student::getScore));
         System.out.println(averagingDouble);
@@ -65,11 +63,9 @@ public class ThreadLocalTest {
         // 和：summingDouble、summingInt、summingLong
         Integer summingInt = students.stream().collect(Collectors.summingInt(Student::getAge));
         System.out.println(summingInt);
-
         //最大值/最小值元素：maxBy、minBy  这两个函数就是求聚合元素中指定比较器中的最大/最小元素。比如，求年龄最大/最小的Student对象：
         Optional<Student> maxBy = students.stream().collect(Collectors.maxBy(Comparator.comparing(Student::getAge)));
         Student student = maxBy.get();
-
         /** 聚合分组
          *  1、聚合元素：toList、toSet、toCollection
          *  这几个函数是将聚合之后的元素，重新封装到队列中，然后返回。比如，得到所有Student的 ID 列表，只需要根据需要的结果类型使用不同的方法即可
@@ -91,13 +87,11 @@ public class ThreadLocalTest {
         Map<String, Student> collect = students.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
         // 但是，如果 id 有重复的，会抛出java.lang.IllegalStateException: Duplicate key异常，所以，为了保险起见，我们需要借助toMap另一个重载方法：
         Map<String, Student> collect2 = students.stream().collect(Collectors.toMap(Student::getId, Function.identity(), (x, y) -> x));
-
         //根据 id 分组的Student的姓名
         Map<String, String> collect1 = students.stream().collect(Collectors.toMap(Student::getId, Student::getName));
 
         // 相同年龄得分最高的Student对象集合
-        Map<Integer, Student> collect3 = students.stream().collect(Collectors.toMap(Student::getAge, Function.identity(), BinaryOperator.maxBy(Comparator.comparing(Student::getAge))));
-
+        Map<Integer, Student> collect3 = students.stream().collect(Collectors.toMap(Student::getAge, Function.identity(), BinaryOperator.maxBy(Comparator.comparing(Student::getScore))));
 
         /**
          * 3、分组：groupingBy、groupingByConcurrent
@@ -123,12 +117,10 @@ public class ThreadLocalTest {
          * reduce
          */
         // 求和
-        students.stream().map(Student::getAge).reduce(0,Integer::sum);
-        students.stream().map(Student::getAge).reduce(Integer::sum).orElse(0);
-
+        Integer reduce = students.stream().map(Student::getAge).reduce(0, Integer::sum);
+        Integer integer1 = students.stream().map(Student::getAge).reduce(Integer::sum).orElse(0);
         // 最大值最小值
-        students.stream().map(Student::getAge).reduce(Integer::min).orElse(0);
-
+        Integer integer = students.stream().map(Student::getAge).reduce(Integer::min).orElse(0);
     }
 
 }
--- src/main/java/dubaolei/Java8/Student.java
+++ src/main/java/dubaolei/Java8/Student.java
@@ -1,7 +1,5 @@
 package dubaolei.Java8;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.time.LocalDate;
 
@@ -12,12 +10,59 @@ import java.time.LocalDate;
  * @Description TODO
  * @createTime 2022年06月30日 16:50:00
  */
-@Data
-@AllArgsConstructor
+
 public class Student {
     private String id;
     private String name;
     private LocalDate birthday;
     private int age;
     private double score;
+
+    public Student(String id, String name, LocalDate birthday, int age, double score) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.age = age;
+        this.score = score;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 }
