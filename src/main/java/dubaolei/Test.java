f216dff59da126dadf43011668a28c3d367a5721
bp.dubaolei <bp.dubaolei@vhallops.com>
2023-03-11 17:50:38 +0800

    测试commit

---
+++ src/main/java/dubaolei/Test.java
@@ -0,0 +1,16 @@
+package dubaolei;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author dubaolei
+ * @version 1.0.0
+ * @ClassName Test.java
+ * @Description TODO
+ * @createTime 2023年03月11日 17:50:00
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
