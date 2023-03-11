16cd887e7ae115a787cc3f4b5dcd75a69d8141fc
bp.dubaolei <bp.dubaolei@vhallops.com>
2023-03-11 17:48:24 +0800

    测试commit

---
+++ src/main/java/dubaolei/TestOne.java
@@ -0,0 +1,15 @@
+package dubaolei;
+
+/**
+ * @author dubaolei
+ * @version 1.0.0
+ * @ClassName TestOne.java
+ * @Description TODO
+ * @createTime 2023年03月11日 17:47:00
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+    }
+}
